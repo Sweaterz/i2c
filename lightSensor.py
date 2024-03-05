9b2c811eabe69066eb6c46df76970171862dbec9
Sweaterz <709218316@qq.com>
2024-03-05 11:03:38 +0800

    优化代码逻辑

--- lightSensor.py
+++ lightSensor.py
@@ -4,17 +4,27 @@ import time
 import BH1715
 import BH1730
 
+def read_sensor_data(sensor, default_value=0):
+    try:
+        return sensor.detect()
+    except Exception as e:
+        print(f"Error reading from {sensor.__name__}: {e}")
+        return default_value
 
 if __name__ == '__main__':
-
-    current_date = time.strftime("%Y-%m-%d", time.localtime())
+    # 可配置部分
     log_format = ".txt"
-    file_name = current_date + log_format
+    file_name = f"{time.strftime('%Y-%m-%d', time.localtime())}{log_format}"
+    sleep_interval = 1  # 数据采集间隔时间
+
     with open(file_name, 'a') as file:
         for i in range(10):
-            lumi15 = BH1715.detect()
-            lumi30 = BH1730.detect()
+            lumi15 = read_sensor_data(BH1715, default_value=-1)
+            lumi30 = read_sensor_data(BH1730, default_value=-1)
             formatted_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-            content = formatted_time + "    BH1715:" + str(lumi15) + "lux    BH1730:" + str(lumi30) + "lux\n"
+            if lumi15 == -1 or lumi30 == -1:
+                content = f"{formatted_time}    ERROR: Fail to get light sensor data. \n"
+            else:
+                content = f"{formatted_time}    BH1715: {lumi15}lux    BH1730: {lumi30}lux\n"
             file.write(content)
-            time.sleep(1)
+            time.sleep(sleep_interval)
